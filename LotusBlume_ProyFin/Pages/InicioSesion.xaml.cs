c61afbc5aca8efaf0b7ba8c376d434a1ae7f7921
Abraham Esaú <abrahamesaucz7@gmail.com>
2025-06-08 22:04:39 -0600

    arreglos del inicio de sesion

--- LotusBlume_ProyFin/Pages/InicioSesion.xaml.cs
+++ LotusBlume_ProyFin/Pages/InicioSesion.xaml.cs
@@ -7,4 +7,18 @@ public partial class InicioSesion : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void OnTogglePasswordVisibility(object sender, EventArgs e)
+    {
+        entryPassword.IsPassword = !entryPassword.IsPassword;
+        var button = (ImageButton)sender;
+        button.Source = entryPassword.IsPassword ? "invisible_contrasena.png" : "visible_contrasena.png";
+    }
+    async public void RecuperarContrasena(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("///RecuperarContrasena");
+    }
+    async public void RegistrarseBoton(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("///RegistroDeCuenta");
+    }
 }
